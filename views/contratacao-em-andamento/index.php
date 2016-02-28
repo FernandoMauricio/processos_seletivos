3da15c4ff000ef54a3c449c4a00998514475e307
Fernando Mauricio <fernando.mauricio@am.senac.br>
2016-02-27 21:12:04 -0400

    Inclusão de Cidades para o processo seletivo

--- views/contratacao-em-andamento/index.php
+++ views/contratacao-em-andamento/index.php
@@ -104,7 +104,7 @@ $gridColumns = [
                         },
                         //ENVIAR PARA CORREÇÃO
                         'correcao' => function ($url, $model) {
-                            return Html::a('<span class="glyphicon glyphicon-repeat"></span> Enviar para Correção', $url, [
+                            return Html::a('<span class="glyphicon glyphicon-repeat"></span> Para Correção', $url, [
                                         'class'=>'btn btn-warning btn-xs',
                                          'data' => [
                                                    'confirm' => 'Você tem certeza que deseja ENVIAR PARA CORREÇÃO essa Solicitação de Contratação?',
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -68,7 +68,9 @@ echo NavX::widget([
             ['label' => 'Configuração', 'items' => [
             '<li class="dropdown-header">Cadastros</li>',
                 ['label' => 'Cargos', 'url' => ['/cargos/index']],
+                ['label' => 'Cidades', 'url' => ['/cidades/index']],
                 ['label' => 'Sistemas', 'url' => ['/sistemas/index']],
+
             ]],
         ]],
 
