ab30904676c2d50b4da5643f44ff2783c9971a53
FernandoMauricio <fernando.mauricio@am.senac.br>
2016-05-05 17:12:15 -0400

    Correção de bugs

--- views/curriculos-admin/view.php
+++ views/curriculos-admin/view.php
@@ -188,6 +188,10 @@ echo DetailView::widget([
 
           <div class="col-md-12"><strong>Ensino Médio: </strong><?php echo $curriculosFormacao->medio_comp ? 'Completo' : 'Incompleto' ?></div>
 
+          <div class="col-md-3"><strong>Ensino Técnico: </strong><?php echo $curriculosFormacao->tecnico ? 'Completo' : 'Incompleto' ?></div>
+
+          <div class="col-md-9"><strong>Curso Técnico: </strong><?php echo $curriculosFormacao->tecnico_area ?></div>
+
           <div class="col-md-3"><strong>Ensino Superior: </strong><?php echo $curriculosFormacao->superior_comp ? 'Completo' : 'Incompleto' ?></div>
 
           <div class="col-md-9"><strong>Curso Graduação: </strong><?php echo $curriculosFormacao->superior_area ?></div>
