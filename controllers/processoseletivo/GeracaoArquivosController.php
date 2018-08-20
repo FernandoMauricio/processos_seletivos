74cd7de0d81e2dc9bd9ba21bde13a121117536e1
Fernando Mauricio <fernando.mauricio@am.senac.br>
2018-08-20 10:08:05 -0400

    Correções de Bugs

--- controllers/processoseletivo/GeracaoArquivosController.php
+++ controllers/processoseletivo/GeracaoArquivosController.php
@@ -56,7 +56,7 @@ class GeracaoArquivosController extends Controller
 
     public function actionImprimir($id) {
 
-    //setlocale(LC_ALL, "pt_BR", "pt_BR.ISO-8859-1", "pt_BR.UTF-8", "portuguese");
+    setlocale(LC_ALL, "pt_BR", "pt_BR.ISO-8859-1", "pt_BR.UTF-8", "portuguese");
 
         $model = $this->findModel($id);
         $modelsItens = $model->geracaoarquivosItens;
