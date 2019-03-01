0284bd0c751b7d1e8c460586333b226827e78836
Fernando Mauricio <fernando.mauricio@am.senac.br>
2019-03-01 16:54:10 -0400

    Versão 2.4
    Implementações
     Pedidos de Contratação, Homologação, Custo e Etapas do Processo
    - Ordenação decrescente dos pedidos de custo, homologação e contratacao e também das etapas do processo.
    - Tela de Contratações Pendentes irão aparecer solicitações com a situação: Recebido pelo GGP e Em correção pelo setor.
    - Tela de Contratações Encerradas irão aparecer também as solicitações canceladas.
    - Incluido a informação na impressão da tela de Pedido de Contratação para contratações especiais.
    - Incluído número do edital na tabela de pontuação geral das avaliação na tela de Etapas do Processo.
     Geração de Arquivos
    - Retirado o requisito de cidade na busca por candidatos na Geração de Arquivos.
    - Implementado a opção para inserção de múltiplos e-emails na Geração de Arquivos.
     Banco de Currículos
    - Melhorias na filtragem do banco de currículos.
    
    Correções
     Envio de Email
    - Corrigido o problema de duplicidade no envio de e-mails ao GGP.
     Etapas do Processo
    - Melhoria na consulta de informações onde é mostrado as Etapas do Processo para os Gerentes.
     Aprovações de Candidatos
    - As informação sobre a aprovação dos candidatos agora aparecerão antes de ser enviado para a gerência solicitante.

--- views/site/index.php
+++ views/site/index.php
@@ -34,26 +34,32 @@ $nome_user    = $session['sess_nomeusuario'];
                 </div>
             </div>
         <div class="panel panel-primary">
-            <div class="panel-heading"><i class="glyphicon glyphicon-star-empty"></i> O que há de novo? - Versão 2.3 - Publicado em 22/06/2018</div>
+            <div class="panel-heading"><i class="glyphicon glyphicon-star-empty"></i> O que há de novo? - Versão 2.4 - Publicado em 01/03/2019</div>
             <div class="panel-body">
-              <h4><b style="color: #337ab7;">Implementações</b></h4>
+            <h4><b style="color: #337ab7;">Implementações</b></h4>
+                <h5><i class="glyphicon glyphicon-tag"></i><b> Pedidos de Contratação, Homologação, Custo e Etapas do Processo</b></h5>
+                    <h5>- Ordenação decrescente dos pedidos de custo, homologação e contratacao e também das etapas do processo.</h5>
+                    <h5>- Tela de Contratações Pendentes irão aparecer solicitações com a situação: <b style="color: #2980b9;">Recebido pelo GGP</b> e <b style="color: #2980b9;">Em correção pelo setor</b>.</h5>
+                    <h5>- Tela de Contratações Encerradas irão aparecer também as <b style="color: #2980b9;">solicitações canceladas</b>.</h5>
+                    <h5>- Incluido a informação na impressão da tela de Pedido de Contratação para contratações especiais.</h5>
+                    <h5>- Incluído número do edital na tabela de pontuação geral das avaliação na tela de Etapas do Processo.</h5>
+
+                <h5><i class="glyphicon glyphicon-tag"></i><b> Geração de Arquivos</b></h5>
+                <h5>- Retirado o requisito de cidade na busca por candidatos na Geração de Arquivos.</h5>
+                <h5>- Implementado a opção para inserção de múltiplos e-emails na Geração de Arquivos.</h5>
+
+                <h5><i class="glyphicon glyphicon-tag"></i><b> Banco de Currículos</b></h5>
+                    <h5>- Melhorias na filtragem do banco de currículos. </h5>
+                <br/ >
+            <h4><b style="color: #337ab7;">Correções</b></h4>
+                <h5><i class="glyphicon glyphicon-tag"></i><b> Envio de Email</b></h5>
+                    <h5>- Corrigido o problema de duplicidade no envio de e-mails ao GGP.</h5>
                 <h5><i class="glyphicon glyphicon-tag"></i><b> Etapas do Processo</b></h5>
-                    <h5>- Incluido a coluna "Prática" para o tipo Administrativo.</h5>
+                    <h5>- Melhoria na consulta de informações onde é mostrado as Etapas do Processo para os Gerentes.</h5>
+                <h5><i class="glyphicon glyphicon-tag"></i><b> Aprovações de Candidatos</b></h5>
+                <h5>- As informação sobre a aprovação dos candidatos agora aparecerão antes de ser enviado para a gerência solicitante.</h5>
 
-                <h5><i class="glyphicon glyphicon-tag"></i><b> Curriculos</b></h5>
-                    <h5>- Implementado os campos <b style="color: #e74c3c;">não-obrigatórios</b> no cadastro de curriculos para que o candidato possa informar seu <b style="color: #2980b9;">Facebook</b> e <b style="color: #2980b9;">Linkedin.</b></h5>
-
-                <h5><i class="glyphicon glyphicon-tag"></i><b> Pedido de Homologação</b></h5>
-                    <h5>- Implementado o botão para homologar o pedido, para que o mesmo possar ser bloqueado para atualizações e/ou exclusões.</h5>
-                    <h5>- Alterado o layout de impressão para paisagem.</h5>
-
-                <h5><i class="glyphicon glyphicon-tag"></i><b> Pedido de Custos</b></h5>
-                    <h5>- Implementado o botão para homologar o pedido de custo, para que o mesmo possar ser bloqueado para atualizações e/ou exclusões.</h5>
-                    <h5>- Alterado o layout de impressão para paisagem.</h5>
-
-                <h5><i class="glyphicon glyphicon-tag"></i><b> Contratação</b></h5>
-                    <h5>- Caso o cargo escolhido seja para docente, o campo nível ficará como <b style="color: #e74c3c;">obrigatório</b>.</h5><br/ >
-            </div>
+            <br/ ></div>
         </div>
     </div>
 </div>
--- views/site/versao.php
+++ views/site/versao.php
@@ -14,6 +14,28 @@ $this->title = 'Processo Seletivo - Senac AM';
     <h1 class="text-center"> Histórico de Versões</h1>
         <div class="body-content">
             <div class="panel panel-danger">
+                <div class="panel-heading"><i class="glyphicon glyphicon-star-empty"></i> Versão 2.3 - Publicado em 22/06/2018</div>
+                <div class="panel-body">
+                  <h4><b style="color: #337ab7;">Implementações</b></h4>
+                    <h5><i class="glyphicon glyphicon-tag"></i><b> Etapas do Processo</b></h5>
+                        <h5>- Incluido a coluna "Prática" para o tipo Administrativo.</h5>
+
+                    <h5><i class="glyphicon glyphicon-tag"></i><b> Curriculos</b></h5>
+                        <h5>- Implementado os campos <b style="color: #e74c3c;">não-obrigatórios</b> no cadastro de curriculos para que o candidato possa informar seu <b style="color: #2980b9;">Facebook</b> e <b style="color: #2980b9;">Linkedin.</b></h5>
+
+                    <h5><i class="glyphicon glyphicon-tag"></i><b> Pedido de Homologação</b></h5>
+                        <h5>- Implementado o botão para homologar o pedido, para que o mesmo possar ser bloqueado para atualizações e/ou exclusões.</h5>
+                        <h5>- Alterado o layout de impressão para paisagem.</h5>
+
+                    <h5><i class="glyphicon glyphicon-tag"></i><b> Pedido de Custos</b></h5>
+                        <h5>- Implementado o botão para homologar o pedido de custo, para que o mesmo possar ser bloqueado para atualizações e/ou exclusões.</h5>
+                        <h5>- Alterado o layout de impressão para paisagem.</h5>
+
+                    <h5><i class="glyphicon glyphicon-tag"></i><b> Contratação</b></h5>
+                        <h5>- Caso o cargo escolhido seja para docente, o campo nível ficará como <b style="color: #e74c3c;">obrigatório</b>.</h5><br/ >
+                </div>
+            </div>
+            <div class="panel panel-danger">
                 <div class="panel-heading"><i class="glyphicon glyphicon-star-empty"></i> Versão 2.2 - Publicado em 11/04/2018</div>
                  <div class="panel-body">
                   <h4><b style="color: #337ab7;">Implementações</b></h4>
